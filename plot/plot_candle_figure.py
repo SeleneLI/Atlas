352fe51d3afcbf5f50ce9ae0376f24d8b1f2e94b
SeleneLI <yueli.cnfr@gmail.com>
2015-12-15 21:34:13 +0100

    Yue modified plot/plot_candle_figure.py

--- plot/plot_candle_figure.py
+++ plot/plot_candle_figure.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # 本script功能：
-# 画关于 candle figure 的各种图
+# 画关于 candledtick chart 的各种图
 __author__ = 'yueli'
 
 from config.config import *
@@ -17,13 +17,30 @@ TARGET_CSV_DIFF = os.path.join(ATLAS_TRACES, 'json2csv', '{0}_{1}.csv'.format(EX
 # 计算差值时的参考 probe
 REF_PROBE = 'FranceIX'
 
+# 存储路径
+FIGURE_PATH = os.path.join(ATLAS_FIGURES_AND_TABLES, EXPERIMENT_NAME, 'candlestick_chart', 'mean_of_RTT_diff_compared_with_{0}'.format(REF_PROBE)) # Needs to change
+
+
+# 画出从 ping_associated_analyzer.difference_calculator 得到的结果
+# 即：以 REF_PROBE 为参考点，其余 probe 与其 RTT 的差值的平均值及方差
+def plot_diff_rtt(target_csv, ref_probe):
+    dict_probe_dest_mean, dict_probe_dest_var = ping_associated_analyzer.difference_calculator(target_csv, ref_probe)
+    for key_probe in dict_probe_dest_mean.keys():
+        dest_list = dict_probe_dest_mean[key_probe].keys()
+        rtt_list = dict_probe_dest_mean[key_probe].values()
+
+        plt.plot(range(1,len(dest_list)+1), rtt_list)
+
+        # 检查是否有 os.path.join(FIGURE_PATH) 存在，不存在的话creat
+        try:
+            os.stat(FIGURE_PATH)
+        except:
+            os.makedirs(FIGURE_PATH)
+
+        plt.savefig(os.path.join(FIGURE_PATH, '{0}_&_{1}.eps'.format(key_probe, REF_PROBE)), dpi=300)
+        # 每画一次图一定要 close 掉 ！！否则会不停叠加 ！！
+        plt.close()
+
 
 if __name__ == "__main__":
-    dict_probe_dest_mean = {}
-    dict_probe_dest_var = {}
-    dict_probe_dest_mean, dict_probe_dest_var = ping_associated_analyzer.difference_calculator(TARGET_CSV_DIFF, REF_PROBE)
-    for key in dict_probe_dest_mean.keys():
-        dest_list = dict_probe_dest_mean[key].keys()
-        rtt_list = dict_probe_dest_mean[key].values()
-        plt.plot(range(1,len(dest_list)+1), rtt_list)
-        plt.show()
+    plot_diff_rtt(TARGET_CSV_DIFF, REF_PROBE)
