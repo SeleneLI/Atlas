db38937304cc61bfdc0c2a6791153e762da65b90
hansomesong <qpsong@gmail.com>
2016-06-27 15:06:04 +0200

    Create a python script to automatically download Alexa top 150 sites and retrieve related IP

--- conduct_measurements/5_probes_to_alexa_top150/ping_and_traceroute.py
+++ conduct_measurements/5_probes_to_alexa_top150/ping_and_traceroute.py
@@ -3,7 +3,7 @@
 # 提供开启 RIPE Atlas 上 ping 和 traceroute 的实验
 # 5 个 probes，编号分别为 22341, 13842, 16958 and anchor 6118
 # destination 是 Alexa top 150, http://www.alexa.com/topsites
-# !!注意!! 因为有些网站不允许被 ping，因而没爬一个网址都可先 ping 一下，确定对方有回复才记录下来成为实验开启的正式 destination
+# !!注意!! 因为有些网站不允许被 ping，因而每爬一个网址都可先 ping 一下，确定对方有回复才记录下来成为实验开启的正式 destination
 #!/usr/bin/python
 
 # RIPE Atlas API demo
@@ -11,186 +11,57 @@
 
 import json
 import requests
+import urllib2
+import csv
+import itertools
 import os
 import sys
 import time
 import calendar
 import math
 from config.config import *
-
-# AUTH = "%s/auth" % os.environ['PWD'] # put the KEY in file named 'auth' under current folder
-AUTH = os.path.join(os.path.dirname(__file__), "auth")
-
-MEASUREMENT_NAME = '5_probes_to_alexa_top150'
-MES_ID_PING_FILE = os.path.join(ATLAS_CONDUCT_MEASUREMENTS, MEASUREMENT_NAME, '{0}_ping_measurement_ids_complete.txt'.format(MEASUREMENT_NAME))
-MES_ID_TRACEROUTE_FILE = os.path.join(ATLAS_CONDUCT_MEASUREMENTS, MEASUREMENT_NAME, '{0}_traceroute_measurement_ids_complete.txt'.format(MEASUREMENT_NAME))
+import socket
 
 
-def ping(target, mes_id_file):
-    requests.packages.urllib3.disable_warnings() #disable urllib3 warnings
-    # Measurement definition
-    #target = 'www.enst.fr'
-    description = 'Ping to %s from probe 22341, 13842, 16958 and anchor 6118' % target
-    af = 4 # can be IPv4 or IPv6
-    type = 'ping'
-    resolve_on_probe = False # Set to 'true' if you want the probe to resolve your target instead of the RIPE Atlas servers.
-    is_oneoff = False # Set this to 'true' to make this measurement a one-off.
-    is_public = True # Set true so that the measurement and results are visible to others
-    interval = 600 # default value is 240 seconds; In normal (not one-off) measurements, this value represents the number of seconds between measurements by a single probe.
-    # if it's of one-off measurement, why there would be a interval?
-    # 60sec is the minimum interval, any value smaller than 60 will cause 400 bad request
-    packets = 3 # defualt 3, number of ping packets sent in each ping meaasurement, must between 1 to 16
-    size = 48 # unkonwn default value; the size of ping request; must between 1 and 2048
-    #Probe description
-    probe_count = 4 # probe numbers
-    probe_selec = 'probes'
-    # can be
-    #area: WW, West, North-Central, South-Central, North-East, South-East
-    #country: ISO two-letter country code, https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-    #prefix: e.g. 193.6.0/8, rumor is that it's not working with prefix option
-    #asn: e.g. 147
-    #probes: comma-separated list of integers as the value, e.g. "1,2,3"
-    #msm (re-use the probes in an existing measurement by specifying the id): e.g. 2310237
-    probe_selec_value = '13842, 22341, 16958, 6118'
-    tag_include = ["system-ipv4-works", "system-anchor"]
-    tag_exclude = ["system-ipv4-doesnt-work", "system-ipv6-doesnt-work",]
-    # there are two types of tags: system assigened ones, see https://atlas.ripe.net/docs/probe-tags/
-    # and user assigned ones, see https://atlas.ripe.net/probes/
-
-    start_time = calendar.timegm(time.strptime('2015-10-22 10:00:00', '%Y-%m-%d %H:%M:%S'))
-    stop_time =  calendar.timegm(time.strptime('2015-10-22 16:00:00', '%Y-%m-%d %H:%M:%S'))
-
-    data = {"definitions": [{ "target": target,
-                               "description": description,
-                               "type": type,
-                               "af": af,
-                               "is_oneoff": is_oneoff,
-                               "resolve_on_probe": resolve_on_probe,
-                               "is_public": is_public,
-                               "interval": interval,
-                               "packets": packets,
-                               "size": size}],
-            "probes": [{ "requested": probe_count,
-                         "type": probe_selec,
-                         "value": probe_selec_value}],
-            "start_time": start_time,
-            "stop_time": stop_time
-    }
-
-    # read key from auth file
-    if not os.path.exists(AUTH):
-        raise CredentialsNotFound(authfile)
-    auth = open(AUTH)
-    key = auth.readline()[:-1]
-    auth.close()
-
-    # create measurement
-    url = "https://atlas.ripe.net/api/v1/measurement/"
-    params = {"key": key}
-    headers = {"Content-Type": "application/json",
-              "Accept": "application/json"}
-    results = requests.post(url=url, params=params, headers=headers, data=json.dumps(data))
-    print results.url
-    print results.status_code
-    results.raise_for_status()
-    measure_id = int(results.json()["measurements"][0])
-    print("Measurement #%s created, please wait the result (it may be long)" % (measure_id))
-    mes_id_file.write(measure_id, '\n')
-
-def traceroute(target, mes_id_file):
-    requests.packages.urllib3.disable_warnings() #disable urllib3 warnings
-    # Measurement definition
-    #target = 'www.enst.fr'
-    description = 'Traceroute to %s from probe 22341, 13842, 16958 and anchor 6118' % target
-    af = 4 # can be IPv4 or IPv6
-    type = 'traceroute'
-    resolve_on_probe = False # Set to 'true' if you want the probe to resolve your target instead of the RIPE Atlas servers.
-    is_oneoff = False # Set this to 'true' to make this measurement a one-off.
-    is_public = True # Set true so that the measurement and results are visible to others
-    interval = 1800 # default value is 240 seconds; In normal (not one-off) measurements, this value represents the number of seconds between measurements by a single probe.
-    # if it's of one-off measurement, why there would be a interval?
-    # 60sec is the minimum interval, any value smaller than 60 will cause 400 bad request
-    packets = 3 # defualt 3, number of ping packets sent in each ping meaasurement, must between 1 to 16
-    size = 48 # unkonwn default value; the size of ping request; must between 1 and 2048
-    #Probe description
-    probe_count = 4 # probe numbers
-    probe_selec = 'probes'
-    # can be
-    #area: WW, West, North-Central, South-Central, North-East, South-East
-    #country: ISO two-letter country code, https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-    #prefix: e.g. 193.6.0/8, rumor is that it's not working with prefix option
-    #asn: e.g. 147
-    #probes: comma-separated list of integers as the value, e.g. "1,2,3"
-    #msm (re-use the probes in an existing measurement by specifying the id): e.g. 2310237
-    probe_selec_value = '13842, 22341, 16958, 6118'
-    tag_include = ["system-ipv4-works", "system-anchor"]
-    tag_exclude = ["system-ipv4-doesnt-work", "system-ipv6-doesnt-work",]
-    # there are two types of tags: system assigened ones, see https://atlas.ripe.net/docs/probe-tags/
-    # and user assigned ones, see https://atlas.ripe.net/probes/
-
-    start_time = calendar.timegm(time.strptime('2015-10-22 10:00:00', '%Y-%m-%d %H:%M:%S'))
-    stop_time =  calendar.timegm(time.strptime('2015-10-22 16:00:00', '%Y-%m-%d %H:%M:%S'))
-
-    protocol="ICMP"
-
-    data = {"definitions": [{ "target": target,
-                               "description": description,
-                               "type": type,
-                               "af": af,
-                               "is_oneoff": is_oneoff,
-                               "resolve_on_probe": resolve_on_probe,
-                               "is_public": is_public,
-                               "interval": interval,
-                               "protocol": protocol,
-                               "packets": packets,
-                               "size": size}],
-            "probes": [{ "requested": probe_count,
-                         "type": probe_selec,
-                         "value": probe_selec_value}],
-            "start_time": start_time,
-            "stop_time": stop_time
-    }
-
-    # read key from auth file
-    if not os.path.exists(AUTH):
-        raise CredentialsNotFound(authfile)
-    auth = open(AUTH)
-    key = auth.readline()[:-1]
-    auth.close()
-
-    # create measurement
-    url = "https://atlas.ripe.net/api/v1/measurement/"
-    params = {"key": key}
-    headers = {"Content-Type": "application/json",
-              "Accept": "application/json"}
-    results = requests.post(url=url, params=params, headers=headers, data=json.dumps(data))
-    print results.url
-    print results.status_code
-    results.raise_for_status()
-    measure_id = int(results.json()["measurements"][0])
-    print("Measurement #%s created, please wait the result (it may be long)" % (measure_id))
-    mes_id_file.write(measure_id, '\n')
 
 if __name__ == "__main__":
-   target_site_l = [ u'Facebook.com', u'Youtube.com', u'Baidu.com', u'Yahoo.com', u'Amazon.com',
-                     u'Wikipedia.org', u'Qq.com', u'Twitter.com', u'Taobao.com', u'Live.com',
-                     u'Sina.com.cn', u'Linkedin.com', u'Yahoo.co.jp', u'Weibo.com', u'Ebay.com',
-                     u'Yandex.ru', u'Hao123.com', u'Vk.com', u'Bing.com', u'Instagram.com',
-                     u'T.co', u'Msn.com', u'Amazon.co.jp',  u'Pinterest.com', u'Aliexpress.com',
-                     u'Ask.com', u'Tmall.com', u'Blogspot.com', u'Wordpress.com', u'Reddit.com',
-                     u'Apple.com', u'Paypal.com', u'Onclickads.net',  u'Mail.ru', u'Tumblr.com',
-                     u'Sohu.com', u'Microsoft.com', u'Imgur.com', u'Xvideos.com', u'Imdb.com',
-                      u'Netflix.com', u'Gmw.cn', u'Fc2.com', u'Amazon.de', u'360.cn',
-                      u'Alibaba.com', u'Go.com', u'Stackoverflow.com',  u'Ok.ru', u'Craigslist.org',
-                      u'Tianya.cn', u'Amazon.co.uk', u'Pornhub.com', u'Rakuten.co.jp', u'Blogger.com',
-                      u'Naver.com', u'Amazon.in', u'Espn.go.com', u'Xhamster.com', u'Flipkart.com'
-    ]
+    # Ref: http://stackoverflow.com/questions/29911507/reading-first-n-lines-of-a-csv-into-a-dictionary
+    # 讲述了如何读取 CSV 文件的前 N 行的例子
+    SITE_NB = 150
+    PARSE = True
+    ALEXA_TOP_SITE_F = 'top-1m.csv'
+    EXP_INPUT_F = 'exp_input.csv'
+    target_site_l = []
+    if PARSE == True:
+        with open(EXP_INPUT_F, 'w') as out_csvfile:
+            spamewriter = csv.writer(out_csvfile, delimiter=',')
+            with open(ALEXA_TOP_SITE_F, 'r') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=',')
+                for row in spamreader:
+                    site_name = row[1].decode('utf-8')
+                    site_ip = ''
+                    try:
+                        site_ip = socket.gethostbyname(site_name)
+                        target_site_l.append([site_name, site_ip])
+                        spamewriter.writerow([site_name, site_ip])
+                        if len(target_site_l) == SITE_NB:
+                            print SITE_NB, "sites have been retrieved. Stop now"
+                            break
+                    except:
+                        print site_name, ': unknown host! Skipped'
 
-    with open(MES_ID_PING_FILE, 'w') as ping_file, open(MES_ID_TRACEROUTE_FILE, 'w') as traceroute_file:
-        target_site_l = [str(e) for e in target_site_l]
-        for target in target_site_l:
-            try:
-                ping(target, ping_file)
-                traceroute(target, traceroute_file)
-            except requests.exceptions.HTTPError:
-                print "Due to unknown cause, cannot launch mesurement for {0}".format(target)
+
+    # convert each URL into Unicode
+    with open(EXP_INPUT_F, 'r') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        for row in spamreader:
+            print row
+
+    # print len(target_site_l), target_site_l
+
+
+
+
+
+
+
